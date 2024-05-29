f367e32bc4bf0e553b3ea7112421c6ce3cf7d1dc
Anakharsis9 <dayana530807@gmail.com>
2024-05-29 20:42:32 +0500

    add base layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,55 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <main class="game__container">
+    <div class="game__board">
+      <div v-for="n in 6" :key="n" class="board__row">
+        <div v-for="n in 'goody'" :key="n" class="board__tile">{{ n }}</div>
+      </div>
+    </div>
+    <div class="keyboard__container">
+      <div class="keyboard__row">
+        <button type="button" class="key__button">q</button>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.game__container {
+  margin-top: 65px;
+  padding-top: 30px;
+  box-sizing: border-box;
+}
+.game__board {
+  width: 330px;
+  height: 400px;
+  display: grid;
+  grid-template-rows: repeat(6, 1fr);
+  gap: 5px;
+  padding: 10px;
+}
+.board__row {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 5px;
+}
+.board__tile {
+  border: 2px solid var(--main-gray);
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  text-transform: uppercase;
+  line-height: 1;
+  font-weight: bold;
+  vertical-align: middle;
+}
+.keyboard__container {
+}
+.keyboard__row {
+}
+.key__button {
+}
+</style>
