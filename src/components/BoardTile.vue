0cd3a799b35cebb76a03c57010d61c63358611c5
Anakharsis9 <dayana530807@gmail.com>
2024-05-30 19:39:26 +0500

    feat: create Tile component

---
+++ src/components/BoardTile.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    letter?: string;
+    color?: "transparent" | "green" | "yellow" | "gray";
+  }>(),
+  {
+    letter: "",
+    color: "transparent"
+  }
+);
+</script>
+
+<template>
+  <div class="board__tile">{{ letter }}</div>
+</template>
+
+<style scoped>
+.board__tile {
+  border: 2px solid var(--main-gray);
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  text-transform: uppercase;
+  line-height: 1;
+  font-weight: bold;
+  vertical-align: middle;
+}
+</style>
